84d7006f9d34dd5b68ef33ed25ed33067cd39d91
Euphorichub <106897467+euphorichubbbb@users.noreply.github.com>
2023-10-30 15:00:23 +0530

    Fix UI of Extension

---
+++ popup.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const updatePopupContent = (summary) => {
+    const summaryElement = document.getElementById('summary');
+    summaryElement.textContent = summary;
+  };
+
+  const fetchPage = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { method: 'fetchPage', tabId: tabs[0].id },
+        (response) => updatePopupContent(response.summary || 'No summary available.')
+      );
+    });
+  };
+
+  const checkButton = document.getElementById('check');
+  checkButton.addEventListener('click', fetchPage);
+});
